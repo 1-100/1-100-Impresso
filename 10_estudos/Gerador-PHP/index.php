52ba81a83ff289e36325154270f01a4ff4a0dfba
Guilherme Vieira <guilhermesv@gmail.com>
2024-03-26 21:26:12 -0300

    Organização

--- 10_estudos/Gerador-PHP/index.php
+++ 10_estudos/Gerador-PHP/index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>12</title>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet"> 
-  <link rel="stylesheet" href="https://1-100.github.io/css/normalize.css">
-  <style>
-    
+<?php include 'parcials/head.php' ?>
 
-
-  </style>
-</head>
 <body>
   <div class="folha">
     <div class="conteudo">
