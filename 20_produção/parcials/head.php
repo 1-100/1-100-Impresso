41f99de5d9df5a16f35ef233362958cb84f0a1e4
Guilherme Vieira <guilhermesv@gmail.com>
2024-03-31 16:20:11 -0300

    Início produção

---
+++ 20_produção/parcials/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>1a100-Miolo</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="https://1-100.github.io/css/normalize.css">
+  <link rel="stylesheet" href="../css/main.css">
+</head>
